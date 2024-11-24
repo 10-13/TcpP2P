c1cfe6da67d15f1e35723a43b49cb19f1e829efc
10-13 <103519265+10-13@users.noreply.github.com>
2024-11-24 19:18:44 +0300

    Adding basic files for SpringApp

---
+++ src/test/java/KilimanJARo/P2P/P2PApplicationTests.java
@@ -0,0 +1,13 @@
+package KilimanJARo.P2P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
