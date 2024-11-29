e875d03a57a72f4e085932b0aa696e449a314538
10-13 <103519265+10-13@users.noreply.github.com>
2024-11-29 19:00:30 +0300

    Adding comments for class to make it easier to understand.

--- src/main/java/KilimanJARo/P2P/client/tunneling/Tunnel.java
+++ src/main/java/KilimanJARo/P2P/client/tunneling/Tunnel.java
@@ -1,16 +1,25 @@
 package KilimanJARo.P2P.client.tunneling;
 
-import jdk.jshell.spi.ExecutionControl;
-
 public class Tunnel {
+    /**
+     * Use some timeout to return null if it failed to connect in some short period of time
+     * @param from Wait for connection from this address
+     * @param to Request connection to this address
+     */
     public static Tunnel Create(String from, String to) {
         throw new RuntimeException("Not implemented.");
     }
 
+    /**
+     * Close all connections
+     */
     public void Close() {
         throw new RuntimeException("Not implemented.");
     }
 
+    /**
+     * @return If from|to where null, returns port where they're redirecting
+     */
     public int Port() {
         throw new RuntimeException("Not implemented.");
     }
