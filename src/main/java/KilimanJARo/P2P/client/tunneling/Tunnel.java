925c76acb6cba530280f3e56af74e68483dfb17b
10-13 <103519265+10-13@users.noreply.github.com>
2024-11-29 18:42:09 +0300

    Adding tunnel template for Tunnel class

---
+++ src/main/java/KilimanJARo/P2P/client/tunneling/Tunnel.java
@@ -0,0 +1,17 @@
+package KilimanJARo.P2P.client.tunneling;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class Tunnel {
+    public static Tunnel Create(String from, String to) {
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public void Close() {
+        throw new RuntimeException("Not implemented.");
+    }
+
+    public int Port() {
+        throw new RuntimeException("Not implemented.");
+    }
+}
