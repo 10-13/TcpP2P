aea17b1f5e9792157d770ff56390053b332902ab
10-13 <103519265+10-13@users.noreply.github.com>
2024-11-30 17:32:39 +0300

    Improving descriptions for new logic

--- src/main/java/KilimanJARo/P2P/client/tunneling/Tunnel.java
+++ src/main/java/KilimanJARo/P2P/client/tunneling/Tunnel.java
@@ -3,8 +3,10 @@ package KilimanJARo.P2P.client.tunneling;
 public class Tunnel {
     /**
      * Use some timeout to return null if it failed to connect in some short period of time
-     * @param from Wait for connection from this address
-     * @param to Request connection to this address
+     * @param from Contains IP and PORT where u need to connect via TCP
+     * @param to Find free port and run async thread to wait for connection on them.
+     *           Check for connected user has IP same as 'to'.
+     *           After connected, need to redirect data on the same thread until Close is called or connection closed.
      */
     public static Tunnel Create(String from, String to) {
         throw new RuntimeException("Not implemented.");
@@ -18,7 +20,7 @@ public class Tunnel {
     }
 
     /**
-     * @return If from|to where null, returns port where they're redirecting
+     * @return Returns port for output connection
      */
     public int Port() {
         throw new RuntimeException("Not implemented.");
